40ce802375cacc3375122d2183958a05bc683b9c
Nandini Neralagi <nandinineralagi@gmail.com>
2024-04-20 20:12:20 -0400

    updates

--- particle_ide_code/tcp_accel_sampler.ino
+++ particle_ide_code/tcp_accel_sampler.ino
@@ -1,47 +1,53 @@
-// This #include statement was automatically added by the Particle IDE.
 #include <SparkFunMMA8452Q.h>
-
-MMA8452Q accel; // MMA8452 QWIIC Accelerometer
+#include "ICM_20948.h"
+#define SERIAL_PORT Serial
+#define WIRE_PORT Wire
+#define AD0_VAL_1 1  
 TCPClient client; // TCP Client
-byte server[] = {172, 26, 1, 41}; // TODO: Server IP, separated by commas instead of periods
+byte server[] = {172,26,23,208}; // TODO: Server IP, separated by commas instead of periods
 char buffer[256]; // rx buffer
 int framesToRead = 0; // samples remaining
 String msg = ""; // tx buffer
 
+ICM_20948_I2C myICM1; // Instance for first sensor
+MMA8452Q accel; // MMA8452 QWIIC Accelerometer
 int led = D7; // on-board LED
 
-void setup()
-{
-  // Make sure your Serial Terminal app is closed before powering your device
-  Serial.begin(9600);
+
+void setup() {
+  SERIAL_PORT.begin(115200);
+  while (!SERIAL_PORT);
+
+  WIRE_PORT.begin();
+  WIRE_PORT.setClock(400000);
+
+  myICM1.begin(WIRE_PORT, AD0_VAL_1);
+  SERIAL_PORT.print("Initialization of sensor 1 returned: ");
+  SERIAL_PORT.println(myICM1.statusString());
   pinMode(led, OUTPUT);
-  
-  // enable accelerometer at 50Hz
-  if (!accel.begin(SCALE_2G, ODR_50)) {
-      Serial.println("Failed to find IMU");
-  }
-  
-  // connect to socket
-  Serial.println("connecting...");
-  
-  if (client.connect(server, 8080))
-  {
-    Serial.println("connected");
-  }
-  else
-  {
-    Serial.println("connection failed");
-  }
+
+    if (!accel.begin(SCALE_2G, ODR_50)) {
+          SERIAL_PORT.println("Failed to find IMU");
+     }
+    if (client.connect(server, 8080))
+     {
+        Serial.println("connected");
+      }
+     else
+      {
+        Serial.println("connection failed");
+    }
 }
 
-void loop()
-{
-  // if frames remaining, add accelerometer data.
+void loop() {
   if (framesToRead > 0) {
-    digitalWrite(led, HIGH);
-    if (accel.available()) {
+   digitalWrite(led, HIGH);
+    if (myICM1.dataReady() && accel.available() ) {
+        myICM1.getAGMT();
         accel.read();
-        msg += String(accel.cx) + " " + String(accel.cy) + " " + String(accel.cz) + "\n";
+        msg += String(accel.cx*1000) + " " + String(accel.cy*1000) + " " + String(accel.cz*1000) + " " + String(myICM1.accX()) 
+        + " " + String(myICM1.accY()) + " " + String(myICM1.accZ()) + " " + String(myICM1.gyrX()) +  " " + String(myICM1.gyrY()) +  " " + String(myICM1.gyrZ())
+        +  " " + String(myICM1.magX()) +  " " + String(myICM1.magY()) +  " " + String(myICM1.magZ()) + "\n";
         framesToRead --;
         
         // if no frames to read left, send reply.
@@ -59,21 +65,72 @@ void loop()
     memset(buffer, 0, 256 * sizeof(char));
     int bytesRead = client.read((uint8_t *)buffer, 256);
     String buf = String(buffer);
-    framesToRead = buf.toInt();
-    Serial.println("Measuring " + String(framesToRead) + " samples...");
-    
-    // 1
-    digitalWrite(led, HIGH);
-    delay(500);
-    digitalWrite(led, LOW);
-    delay(500);
-    
-    // 2
-    digitalWrite(led, HIGH);
-    delay(500);
-    digitalWrite(led, LOW);
-    delay(500);
+    // if (buf == "trigger_function") {
+    //   triggerFunction();
+    // }
+    // else {
+      framesToRead = buf.toInt();
+      Serial.println("Measuring " + String(framesToRead) + " samples...");
+      // 1
+      digitalWrite(led, HIGH);
+      delay(500);
+      digitalWrite(led, LOW);
+      delay(500);
+      
+      // 2
+      digitalWrite(led, HIGH);
+      delay(500);
+      digitalWrite(led, LOW);
+      delay(500);
+    //}
   } else { // if no available recv and no frames to read, inactive.
       digitalWrite(led, LOW);
+  }
+}
+
+void triggerFunction() {
+    if (Particle.connected()) {
+        Particle.publish("push-notification", "it has activated");
+    }
+    else{
+        Serial.print("could not push");
+    }
+}
+
+
+
+void readAndPrintData(){
+    if (myICM1.dataReady() && accel.available()) {
+    myICM1.getAGMT();
+    accel.read();
+    SERIAL_PORT.print(myICM1.accX(), 3);
+    SERIAL_PORT.print(",");
+    SERIAL_PORT.print(accel.cx*1000, 3);
+    SERIAL_PORT.print(",");
+    SERIAL_PORT.print(myICM1.accY(), 3);
+    SERIAL_PORT.print(",");
+    SERIAL_PORT.print(accel.cy*1000, 3);
+    SERIAL_PORT.print(",");
+    SERIAL_PORT.print(myICM1.accZ(), 3);
+    SERIAL_PORT.print(",");
+    SERIAL_PORT.print(accel.cz*1000, 3);
+    SERIAL_PORT.print(",");
+    SERIAL_PORT.print(myICM1.gyrX());
+    SERIAL_PORT.print(",");
+    SERIAL_PORT.print(myICM1.gyrY());
+    SERIAL_PORT.print(",");
+    SERIAL_PORT.print(myICM1.gyrZ());
+    SERIAL_PORT.print(",");
+    SERIAL_PORT.print(myICM1.magX());
+    SERIAL_PORT.print(",");
+    SERIAL_PORT.print(myICM1.magY());
+    SERIAL_PORT.print(",");
+    SERIAL_PORT.print(myICM1.magZ());
+    SERIAL_PORT.print(",");
+    SERIAL_PORT.print(myICM1.temp());
+    Serial.println();
+  }
+  else{
+      SERIAL_PORT.println("Waiting for data");
   }
 }
--- particle_server_code/particle_server.py
+++ particle_server_code/particle_server.py
@@ -24,6 +24,9 @@ def extract_ax(arr):
     return arr[:, 0:1].flatten()
 def extract_ay(arr):
     return arr[:, 1:2].flatten()
+def extract_az(arr):
+    return arr[:, 2:3].flatten()
+
 # Returns tuple of arrays of x, y peaks
 def get_extrema(ax, ay):
     neg_ax = np.negative(ax)
@@ -58,46 +61,48 @@ def get_feature_conditional(peaks, prominences, mode):
         return np.average(np.diff(prominences[0]))
 
 def featurize(arr):
-	### amy
-	ax = median_filter(extract_ax(arr))
-	ay = median_filter(extract_ay(arr))
-	(ax_peaks, ax_mins, ay_peaks, ay_mins) = get_extrema(ax, ay)
-	(ax_peak_prominence, ax_min_prominence, ay_peak_prominence, ay_min_prominence) = get_prominence(ax, ax_peaks, ax_mins, ay, ay_peaks, ay_mins)
-	fvec = []
-	fvec.append(ax_peaks[0].shape[0]) # number of ax peaks
-	fvec.append(ay_peaks[0].shape[0]) # number of ay peaks
-	fvec.append(get_feature_conditional(ax_peaks, ax_peak_prominence, 'AVG')) # average prominence of ax maxima
-	fvec.append(get_feature_conditional(ay_peaks, ay_peak_prominence, 'AVG')) # average prominence of ay maxima
-	
-	### nandini
-	X_cord = arr[:, 0]
-	Y_cord = arr[:, 1]
-	Z_cord = arr[:, 2]
-	X_fft = np.abs(fft(X_cord))
-	Y_fft = np.abs(fft(Y_cord))
-	Z_fft = np.abs(fft(Z_cord))
-	fv = []
-	fv.append(np.mean(X_fft))
-	fv.append(np.mean(Y_fft))
-	fv.append(np.mean(Z_fft))
-	fv.append(np.max(X_fft))
-	fv.append(np.max(Y_fft))
-	
-	### alejandro
-	features = []
-	# Standard Deviation
-	features.append(np.std(arr, axis=0))
-	# Mean of absolute values
-	features.append(np.mean(np.abs(arr), axis=0))
-	# Max-Min difference
-	features.append(np.max(arr, axis=0) - np.min(arr, axis=0))
-	# Sum of absolute values
-	features.append(np.sum(np.abs(arr), axis=0))
-	# Sum of squares
-	features.append(np.sum(np.square(arr), axis=0))
-	features = np.concatenate(features)
+    return arr.flatten()
 
-	return np.concatenate((fvec,fv,features))
+	### amy
+	# ax = median_filter(extract_ax(arr))
+	# ay = median_filter(extract_ay(arr))
+	# (ax_peaks, ax_mins, ay_peaks, ay_mins) = get_extrema(ax, ay)
+	# (ax_peak_prominence, ax_min_prominence, ay_peak_prominence, ay_min_prominence) = get_prominence(ax, ax_peaks, ax_mins, ay, ay_peaks, ay_mins)
+	# fvec = []
+	# fvec.append(ax_peaks[0].shape[0]) # number of ax peaks
+	# fvec.append(ay_peaks[0].shape[0]) # number of ay peaks
+	# fvec.append(get_feature_conditional(ax_peaks, ax_peak_prominence, 'AVG')) # average prominence of ax maxima
+	# fvec.append(get_feature_conditional(ay_peaks, ay_peak_prominence, 'AVG')) # average prominence of ay maxima
+	
+	# ### nandini
+	# X_cord = arr[:, 0]
+	# Y_cord = arr[:, 1]
+	# Z_cord = arr[:, 2]
+	# X_fft = np.abs(fft(X_cord))
+	# Y_fft = np.abs(fft(Y_cord))
+	# Z_fft = np.abs(fft(Z_cord))
+	# fv = []
+	# fv.append(np.mean(X_fft))
+	# fv.append(np.mean(Y_fft))
+	# fv.append(np.mean(Z_fft))
+	# fv.append(np.max(X_fft))
+	# fv.append(np.max(Y_fft))
+	
+	# ### alejandro
+	# features = []
+	# # Standard Deviation
+	# features.append(np.std(arr, axis=0))
+	# # Mean of absolute values
+	# features.append(np.mean(np.abs(arr), axis=0))
+	# # Max-Min difference
+	# features.append(np.max(arr, axis=0) - np.min(arr, axis=0))
+	# # Sum of absolute values
+	# features.append(np.sum(np.abs(arr), axis=0))
+	# # Sum of squares
+	# features.append(np.sum(np.square(arr), axis=0))
+	# features = np.concatenate(features)
+
+	# return np.concatenate((fvec,fv,features))
 
 # This is the part that trains a classifier
 def train_ml_classifier(): 
@@ -134,10 +139,11 @@ def parse_data_stream(data_stream):
 				values.append(float(val))
 			except ValueError:
 				break
-		if (len(values) == 3):
+		if (len(values) == 12):
 			data_vals.append(values)
 	
 	data_vals = np.array(data_vals)
+	print(data_vals)
 	return data_vals
 
 app = Flask(__name__)
@@ -149,7 +155,7 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 print ("Socket successfully created") 
 
 # TODO: update with your IP here
-s.bind(('172.26.1.41', port))
+s.bind(('172.26.23.208', port))
 print ("socket binded to %s" %(port))
 
 s.listen(5)      
@@ -201,7 +207,11 @@ def index():
 			if gesture not in gesture_dict.keys():
 				gesture_dict[gesture] = []
 			gesture_dict[gesture].append(parse_data_stream(stream))
-			# np.save('gesture_dict.npy',  gesture_dict) 
+			# print(gesture_dict)
+   			# gesture_dict1 = {"Posture 1 Good": gesture_dict["Posture 1 Good"], }
+			# np.save('gesture_dict1.npy',  gesture_dict1) 
+			# np.save('gesture_dict2.npy',  gesture_dict2) 
+			np.save('gesture_dict.npy',  gesture_dict) 
 		
 		# if testing, receive new data stream and make inference
 		elif request.form["submit"] == "Test":
